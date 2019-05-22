7f6beddbfdc7e22707e8470bafb2f001a538fc68
oRlex <orlexfed@gmail.com>
2019-05-22 19:11:55 +0300

    Updates

---
+++ precache-manifest.fe0e4014863d6b6d246fb93f66fc40a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640bca67b20aa6a36a5e148d094fcd6f",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "be698b2136246f47e02b",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contact-manager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "be698b2136246f47e02b",
+    "url": "/contact-manager/static/js/main.4bce3fb1.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
